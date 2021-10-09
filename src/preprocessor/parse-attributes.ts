5b068b172a07d32f661e6053f4bc1fc4e20632ea
Chris Han <christopher.f.han@gmail.com>
2021-10-08 19:20:48 -0500

    Strip newlines before parsing attribute, fixes #14

--- src/preprocessor/parse-attributes.ts
+++ src/preprocessor/parse-attributes.ts
@@ -1,25 +1,27 @@
 import { HTMLElement, parse } from 'node-html-parser';
 
-export default function parseAttributes(element: string): Record<string, string> {
-    if (!element) {
-        return {};
-    }
+export default function parseAttributes(
+  element: string,
+): Record<string, string> {
+  if (!element) {
+    return {};
+  }
 
-    const root = parse(element);
-    if (!root?.firstChild) {
-        return {};
-    }
+  const root = parse(element.replace(/[\r\n]+/g, ' '));
+  if (!root?.firstChild) {
+    return {};
+  }
 
-    const node = root.firstChild as HTMLElement;
-    if (!node?.attributes) {
-        return {};
-    }
+  const node = root.firstChild as HTMLElement;
+  if (!node?.attributes) {
+    return {};
+  }
 
-    return Object.entries(node.attributes).reduce<Record<string, string>>(
-        (rv, [attr, val]) => {
-            rv[attr] = val === '' ? attr : val;     // so empty value attributes can be truthy
-            return rv;
-        },
-        {}
-    );
+  return Object.entries(node.attributes).reduce<Record<string, string>>(
+    (rv, [attr, val]) => {
+      rv[attr] = val === '' ? attr : val; // so empty value attributes can be truthy
+      return rv;
+    },
+    {},
+  );
 }
--- tests/preprocessor/parse-attributes.spec.ts
+++ tests/preprocessor/parse-attributes.spec.ts
@@ -1,64 +1,100 @@
 import parseAttributes from '../../src/preprocessor/parse-attributes';
+import * as parser from 'node-html-parser';
 
 describe('parseAttributes', () => {
+  let parseSpy: jest.SpyInstance;
 
-    it('returns empty if nothing is passed', () => {
-        expect(parseAttributes('')).toEqual({});
+  beforeEach(() => {
+    parseSpy = jest.spyOn(parser, 'parse');
+  });
+
+  afterEach(() => {
+    parseSpy.mockClear();
+  });
+
+  it('returns empty if nothing is passed', () => {
+    expect(parseAttributes('')).toEqual({});
+  });
+
+  it('parses a string attribute', () => {
+    expect(parseAttributes('<Image src="images/test.jpg">')).toEqual({
+      src: 'images/test.jpg',
     });
 
-    it('parses a string attribute', () => {
-        expect(parseAttributes(
-            '<Image src="images/test.jpg">'
-        )).toEqual({
-            src: 'images/test.jpg',
-        });
+    expect(parseAttributes("<Image src='images/test.jpg'>")).toEqual({
+      src: 'images/test.jpg',
+    });
+  });
 
-        expect(parseAttributes(
-            '<Image src=\'images/test.jpg\'>'
-        )).toEqual({
-            src: 'images/test.jpg',
-        });
+  it('parses an expression attribute', () => {
+    expect(parseAttributes('<Image alt={altText}>')).toEqual({
+      alt: '{altText}',
+    });
+  });
+
+  it('parses a numeric attribute', () => {
+    expect(parseAttributes('<Image width="150">')).toEqual({
+      width: '150',
     });
 
-    it('parses an expression attribute', () => {
-        expect(parseAttributes(
-            '<Image alt={altText}>'
-        )).toEqual({
-            alt: '{altText}',
-        });
+    expect(parseAttributes('<Image width=150>')).toEqual({
+      width: '150',
+    });
+  });
+
+  it('parses an empty value attribute', () => {
+    expect(parseAttributes('<Image immediate>')).toEqual({
+      immediate: 'immediate',
+    });
+  });
+
+  it('parses multiple', () => {
+    expect(
+      parseAttributes(
+        '<Image src="images/test.jpg" alt={altText} class="red" width="150" immediate>',
+      ),
+    ).toEqual({
+      src: 'images/test.jpg',
+      alt: '{altText}',
+      class: 'red',
+      width: '150',
+      immediate: 'immediate',
+    });
+  });
+
+  it('strips unix line breaks before parsing', () => {
+    expect(
+      parseAttributes(
+        '<Image\n\tsrc="images/test.jpg"\n\talt={altText}\n\tclass="red"\n\twidth="150"\n\timmediate>',
+      ),
+    ).toEqual({
+      src: 'images/test.jpg',
+      alt: '{altText}',
+      class: 'red',
+      width: '150',
+      immediate: 'immediate',
     });
 
-    it('parses a numeric attribute', () => {
-        expect(parseAttributes(
-            '<Image width="150">'
-        )).toEqual({
-            width: '150',
-        });
+    expect(parseSpy).toHaveBeenCalledWith(
+      '<Image \tsrc="images/test.jpg" \talt={altText} \tclass="red" \twidth="150" \timmediate>',
+    );
+  });
 
-        expect(parseAttributes(
-            '<Image width=150>'
-        )).toEqual({
-            width: '150',
-        });
+  it('strips windows line breaks before parsing', () => {
+    expect(
+      parseAttributes(
+        '<Image\r\nsrc="images/test2.jpg"\r\nalt={altText}\r\nclass="red"\r\nwidth="150"\r\nimmediate\r\n>',
+      ),
+    ).toEqual({
+      src: 'images/test2.jpg',
+      alt: '{altText}',
+      class: 'red',
+      width: '150',
+      immediate: 'immediate',
     });
 
-    it('parses an empty value attribute', () => {
-        expect(parseAttributes(
-            '<Image immediate>'
-        )).toEqual({
-            immediate: 'immediate',
-        });
-    });
-
-    it('parses multiple', () => {
-        expect(parseAttributes(
-            '<Image src="images/test.jpg" alt={altText} class="red" width="150" immediate>'
-        )).toEqual({
-            src: 'images/test.jpg',
-            alt: '{altText}',
-            class: 'red',
-            width: '150',
-            immediate: 'immediate',
-        });
-    });
-})
+    expect(parseSpy).toHaveBeenCalledWith(
+      '<Image src="images/test2.jpg" alt={altText} class="red" width="150" immediate >',
+    );
+  });
+});

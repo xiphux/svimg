87265b3c754a65e6edbf040d3152085e960e23d2
Chris Han <christopher.f.han@gmail.com>
2022-02-12 20:33:24 -0600

    Add JSDoc

--- src/Image.ts
+++ src/Image.ts
@@ -1,13 +1,50 @@
 import { SvelteComponentTyped } from 'svelte';
 
 export interface ImageProps {
+  /**
+   * Image url
+   */
   src: string;
+
+  /**
+   * Alternate text for the image
+   */
   alt?: string;
+
+  /**
+   * CSS classes to apply to image
+   */
   class?: string;
+
+  /**
+   * Resize image to specified width in pixels
+   */
   width?: number | string;
+
+  /**
+   * Set to true to disable lazy-loading
+   *
+   * @default false
+   */
   immediate?: boolean;
+
+  /**
+   * Amount of blur to apply to placeholder
+   *
+   * @default 40
+   */
   blur?: number | string;
+
+  /**
+   * Quality of the resized images
+   *
+   * @default sharp's default quality
+   */
   quality?: number | string;
 }
 
+/**
+ * Image lazy loading Svelte component
+ * for the svimg package
+ */
 export default class Image extends SvelteComponentTyped<ImageProps> {}
--- src/preprocessor/image-preprocessor.ts
+++ src/preprocessor/image-preprocessor.ts
@@ -4,13 +4,61 @@ import replaceAsync from 'string-replace-async';
 import processImageElement from './process-image-element';
 
 export interface ImagePreprocessorOptions {
+  /**
+   * The static asset directory where
+   * image urls are retrieved from
+   */
   inputDir: string;
+
+  /**
+   * The output directory where resized image files
+   * should be written to. This should usually be a
+   * subdirectory within the normal static asset directory
+   */
   outputDir: string;
+
+  /**
+   * The public path that images will be served from.
+   * This will be prepended to the src url during preprocessing.
+   *
+   * The default behavior will work for most use cases,
+   * where the outputDir is a subdirectory of the inputDir
+   * static asset directory and the site is served from the
+   * root of the domain.
+   *
+   * This can be overridden for more advanced use cases, such as
+   * a site served from a subdirectory of the domain or for
+   * images served from a separate domain such as a CDN or
+   * static asset domain.
+   *
+   * @default The outputDir relative to the inputDir static asset directory
+   */
   publicPath?: string;
+
+  /**
+   * Whether to generate WebP versions of images
+   * in addition to the original image formats
+   *
+   * @default true
+   */
   webp?: boolean;
+
+  /**
+   * Whether to generate AVIF versions of images
+   * in addition to the original image formats
+   *
+   * @default true
+   */
   avif?: boolean;
 }
 
+/**
+ * Image processing Svelte preprocessor
+ * for the svimg package
+ *
+ * @param options Image preprocessor options
+ * @returns Svelte preprocessor
+ */
 export default function imagePreprocessor(
   options?: ImagePreprocessorOptions,
 ): PreprocessorGroup {

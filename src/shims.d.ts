018ce7f6b821463c59049da23f97bb600fb8610b
Chris Han <christopher.f.han@gmail.com>
2022-02-12 19:10:21 -0600

    Remove unnecessary typescript declaration

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,18 +1,10 @@
 declare module 'md5-file' {
-    var md5Promise: {
-        (filename: string): Promise<string>;
-        sync: (filename: string) => string;
-    };
+  var md5Promise: {
+    (filename: string): Promise<string>;
+    sync: (filename: string) => string;
+  };
 
-    export = md5Promise;
-}
-
-declare module 'mini-svg-data-uri' {
-    var svgDataUri: {
-        (svg: string): string;
-        toSrcset: (svg: string) => string;
-    };
-    export = svgDataUri;
+  export = md5Promise;
 }
 
 declare module '*.svelte';

e274730973df80d6421f9c09ff415830138a5888
Chris Han <christopher.f.han@gmail.com>
2022-05-10 21:36:53 -0500

    Close all Images properly in parse-attributes test

--- tests/preprocessor/parse-attributes.spec.ts
+++ tests/preprocessor/parse-attributes.spec.ts
@@ -17,11 +17,11 @@ describe('parseAttributes', () => {
   });
 
   it('parses a string attribute', () => {
-    expect(parseAttributes('<Image src="images/test.jpg">')).toEqual({
+    expect(parseAttributes('<Image src="images/test.jpg" />')).toEqual({
       src: 'images/test.jpg',
     });
 
-    expect(parseAttributes("<Image src='images/test.jpg'>")).toEqual({
+    expect(parseAttributes("<Image src='images/test.jpg' />")).toEqual({
       src: 'images/test.jpg',
     });
   });
@@ -33,17 +33,17 @@ describe('parseAttributes', () => {
   });
 
   it('parses a numeric attribute', () => {
-    expect(parseAttributes('<Image width="150">')).toEqual({
+    expect(parseAttributes('<Image width="150" />')).toEqual({
       width: '150',
     });
 
-    expect(parseAttributes('<Image width=150>')).toEqual({
+    expect(parseAttributes('<Image width=150 />')).toEqual({
       width: '150',
     });
   });
 
   it('parses an empty value attribute', () => {
-    expect(parseAttributes('<Image immediate>')).toEqual({
+    expect(parseAttributes('<Image immediate />')).toEqual({
       immediate: 'immediate',
     });
   });
@@ -51,7 +51,7 @@ describe('parseAttributes', () => {
   it('parses multiple', () => {
     expect(
       parseAttributes(
-        '<Image src="images/test.jpg" alt={altText} class="red" width="150" immediate>',
+        '<Image src="images/test.jpg" alt={altText} class="red" width="150" immediate />',
       ),
     ).toEqual({
       src: 'images/test.jpg',
@@ -65,7 +65,7 @@ describe('parseAttributes', () => {
   it('strips unix line breaks before parsing', () => {
     expect(
       parseAttributes(
-        '<Image\n\tsrc="images/test.jpg"\n\talt={altText}\n\tclass="red"\n\twidth="150"\n\timmediate>',
+        '<Image\n\tsrc="images/test.jpg"\n\talt={altText}\n\tclass="red"\n\twidth="150"\n\timmediate\n\t/>',
       ),
     ).toEqual({
       src: 'images/test.jpg',
@@ -76,14 +76,14 @@ describe('parseAttributes', () => {
     });
 
     expect(parseSpy).toHaveBeenCalledWith(
-      '<Image \tsrc="images/test.jpg" \talt={altText} \tclass="red" \twidth="150" \timmediate>',
+      '<Image \tsrc="images/test.jpg" \talt={altText} \tclass="red" \twidth="150" \timmediate \t/>',
     );
   });
 
   it('strips windows line breaks before parsing', () => {
     expect(
       parseAttributes(
-        '<Image\r\nsrc="images/test2.jpg"\r\nalt={altText}\r\nclass="red"\r\nwidth="150"\r\nimmediate\r\n>',
+        '<Image\r\nsrc="images/test2.jpg"\r\nalt={altText}\r\nclass="red"\r\nwidth="150"\r\nimmediate\r\n/>',
       ),
     ).toEqual({
       src: 'images/test2.jpg',
@@ -94,7 +94,7 @@ describe('parseAttributes', () => {
     });
 
     expect(parseSpy).toHaveBeenCalledWith(
-      '<Image src="images/test2.jpg" alt={altText} class="red" width="150" immediate >',
+      '<Image src="images/test2.jpg" alt={altText} class="red" width="150" immediate />',
     );
   });
 });

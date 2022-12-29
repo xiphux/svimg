d014991a1e621b332a9335ce29cfc832128b2fbd
Chris Han <christopher.f.han@gmail.com>
2022-12-28 20:48:33 -0600

    Treat rollup config as esm

---
+++ rollup.config.mjs
@@ -0,0 +1,44 @@
+import typescript from 'rollup-plugin-typescript2';
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
+const entryPoints = [
+  { entry: 'index', formats: ['cjs', 'es'] },
+  { entry: 'process', formats: ['cjs', 'es'] },
+  {
+    entry: 's-image',
+    formats: ['iife'],
+    extraPlugins: [
+      svelte({
+        compilerOptions: {
+          customElement: true,
+        },
+      }),
+      resolve(),
+    ],
+  },
+];
+
+export default [
+  ...entryPoints.map((entryPoint) => ({
+    input: `src/${entryPoint.entry}.ts`,
+    output: entryPoint.formats.map((format) => ({
+      file: `dist/${entryPoint.entry}.${format == 'es' ? 'es.js' : 'js'}`,
+      format,
+    })),
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({
+        typescript: require('typescript'),
+      }),
+      ...(entryPoint.extraPlugins || []),
+    ],
+  })),
+];
